904b7616357b9b72318b2083adcbd9ef26140ab2
Dabasan <43123127+Dabasan@users.noreply.github.com>
2020-05-19 10:45:45 +0900

    Initial commit

---
+++ src/main/java/com/github/dabasan/jassimp/AiConfig.java
@@ -0,0 +1,15 @@
+/*
+ * $Revision$
+ * $Date$
+ */
+package com.github.dabasan.jassimp;
+
+
+/**
+ * Configuration interface for assimp importer.<p>
+ * 
+ * This class is work-in-progress
+ */
+public class AiConfig {
+
+}
